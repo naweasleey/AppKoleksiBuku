f8b6caa0a8385765b64759b2943b38664c400b23
naweasleey <nailyna1212@gmail.com>
2023-02-09 22:58:45 +0700

    Add project files.

---
+++ AppKoleksiBuku/Models/Enum.cs
@@ -0,0 +1,15 @@
+﻿namespace AppKoleksiBuku.Models
+{
+    public enum Buku
+    {
+        ActionOnePiece,
+        ActionBlackClover,
+        ActionJujutsuKaisen,
+        NovelAndreaHirata,
+        NovelJKRowling,
+        NovelTereLiye,
+        SejarahIndonesia,
+        SejarahYunani,
+        SejarahJepang
+    }
+}
---
+++ AppKoleksiBuku/Models/KoleksiBuku.cs
@@ -0,0 +1,12 @@
+﻿namespace AppKoleksiBuku.Models
+{
+    public class KoleksiBuku
+    {
+        public Buku Buku { get; set; }
+
+        public KoleksiBuku(Buku buku)
+        {
+            Buku = buku;
+        }
+    }
+}
